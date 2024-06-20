9f24673709552d4f1d0cf946057fbe2225fde528
Bruno Racobaldo Santos <60931988+racoba@users.noreply.github.com>
2024-06-20 15:49:54 -0300

    MAIN-First version of updates

--- PlayerRL.py
+++ PlayerRL.py
@@ -23,35 +23,68 @@ class RLAgent:
     self.current_input = None
     self.current_output = None    
     
-  def extract_rl_state(self, your_hand):
-    # versão bem básica onde apenas verificamos se o total
-    # em nossa mão é maior que 11, assim podemos
-    # ter uma característica para indicar se tem
-    # uma chance de 'estourar' a mão mas isso 
-    # não leva em conta várias pontos importantes 
-    return (int(calculate_hand_value(your_hand) > 11),);
+  # def extract_rl_state(self, your_hand):
+  #   # versão bem básica onde apenas verificamos se o total
+  #   # em nossa mão é maior que 11, assim podemos
+  #   # ter uma característica para indicar se tem
+  #   # uma chance de 'estourar' a mão mas isso 
+  #   # não leva em conta várias pontos importantes z
+  #   return (int(calculate_hand_value(your_hand) > 11),);
+
+  def extract_rl_state(self, your_hand, dealer_first_card):
+    player_value = calculate_hand_value(your_hand)
+    dealer_value = calculate_hand_value([dealer_first_card])
+    ace_count = sum(1 for card in your_hand if card.value == 'ace')
+    
+    state = (player_value, dealer_value, ace_count)
+    return state
  
+  # def choose_action(self, state):
+  #   #
+  #   # Pode ser melhorado!
+  #   #
+  #   if not state:
+  #     if np.random.uniform(0, 1) < self.epsilon:
+  #       # Explore: Choose a random action
+  #       action = np.random.choice(self.action_list)
+  #     else:
+  #       # Exploit: Choose the action with the maximum Q-value
+  #       action = self.action_list[np.argmax(self.Q[state])]
+  #   else:
+  #     return "hit"
+  #   return action
+  
+
   def choose_action(self, state):
-    #
-    # Pode ser melhorado!
-    #
-    if not state:
-      if np.random.uniform(0, 1) < self.epsilon:
-        # Explore: Choose a random action
-        action = np.random.choice(self.action_list)
-      else:
-        # Exploit: Choose the action with the maximum Q-value
-        action = self.action_list[np.argmax(self.Q[state])]
+    if np.random.uniform(0, 1) < self.epsilon:
+        action = np.random.choice(self.action_list)  
     else:
-      return "hit"
+        if state in self.Q:
+            action = self.action_list[np.argmax(self.Q[state])]
+        else:
+            action = "hit"  
     return action
     
     
-  def update_qtable(self, state, action, reward, next_state):
+  # def update_qtable(self, state, action, reward, next_state):
+  #   alp = self.alpha
+  #   gam = self.gamma
+  #   action_index = self.action_list.index(action)
+  #   self.Q[state][action_index] = (1 - alp) * self.Q[state][action_index] + alp * (reward + gam * np.max(self.Q[next_state]))  
+
+  def update_qtable(self, state, action, reward, next_state, player_hand, dealer_first_card):
     alp = self.alpha
     gam = self.gamma
     action_index = self.action_list.index(action)
-    self.Q[state][action_index] = (1 - alp) * self.Q[state][action_index] + alp * (reward + gam * np.max(self.Q[next_state]))  
+    max_next_q = np.max(self.Q[next_state]) if next_state in self.Q else 0.0
+    
+    if action == "stop" and (calculate_hand_value(player_hand) > 17 and not any(card.value == 'ace' for card in player_hand)):
+        reward += 1  
+    elif action == "hit" and (calculate_hand_value(player_hand) < 17 and calculate_hand_value([dealer_first_card]) in [10, 9, 8, 7]):
+        reward -= 1  
+        
+    self.Q[state][action_index] += self.alp * (reward + self.gam * max_next_q - self.Q[state][action_index])
+
     
   # Essa função toma a decisão após observar
   # o estado observável do campo
@@ -65,17 +98,30 @@ class RLAgent:
     return choice
 
   # Essa função deveria atualiza QTable
+  # def result(self, your_hand, dealer_first_card, decision, reward, is_not_done):
+  #   player_hand = [d for d in your_hand]
+  #   game_status = "still going" if is_not_done else "is done" 
+  #   print(f"{your_hand=}")
+  #   state = self.extract_rl_state(your_hand=your_hand[:-1])
+  #   next_state = self.extract_rl_state(your_hand=your_hand)
+  #   self.update_qtable(state, decision, reward, next_state)
+  #   self.print_q_table(self.Q)
+  #   print(f"Your hand ({calculate_hand_value(your_hand)}) after decision '{decision}' with {reward=} and game {game_status}")
+
+  #   print("======== End of turn =======")    
+
   def result(self, your_hand, dealer_first_card, decision, reward, is_not_done):
-    player_hand = [d for d in your_hand]
+    state = self.extract_rl_state(your_hand)
+    next_state = self.extract_rl_state(your_hand + [dealer_first_card])
+
     game_status = "still going" if is_not_done else "is done" 
     print(f"{your_hand=}")
-    state = self.extract_rl_state(your_hand=your_hand[:-1])
-    next_state = self.extract_rl_state(your_hand=your_hand)
-    self.update_qtable(state, decision, reward, next_state)
+
+    self.update_qtable(state, decision, reward, next_state, your_hand, dealer_first_card)
     self.print_q_table(self.Q)
     print(f"Your hand ({calculate_hand_value(your_hand)}) after decision '{decision}' with {reward=} and game {game_status}")
 
-    print("======== End of turn =======")    
+    print("======== End of turn =======") 
   
   def print_q_table(self, Q):
     for k,v in Q.items():
